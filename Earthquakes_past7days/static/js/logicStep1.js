7b864de79a7f8bdc63419ad40ad633a661e0ccfe
Xu tian <xutian@Xus-iMac.lan>
2021-05-26 15:28:27 -0500

    uploading mapping earthquakes step 1.

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -18,13 +18,13 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 // Create a base layer that holds both maps.
 let baseMaps={
     "Streets": streets,
-    "Satellite Streets": satelliteStreets
+    "Satellite": satelliteStreets
 };
 
 // Create the map object with center, zoom level and default layer.
 let map=L.map("mapid",{
-    center:[43.7,-79.3],
-    zoom:11,
+    center:[39.5,-98.5],
+    zoom:3,
     layers:[streets]
 })
 
@@ -33,7 +33,7 @@ L.control.layers(baseMaps).addTo(map);
 
 // Accessing the Toronto airline routes GeoJSON URL.
 
-let torontoHoods="https://raw.githubusercontent.com/cathyxuxuxu/Mapping_Earthquakes/main/torontoNeighborhoods.json"
+let earthquakeData="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
 // Create a style for the lines.
 let myStyle={
@@ -44,17 +44,10 @@ let myStyle={
 
 // Grabbing our GeoJSON data.
 
-d3.json(torontoHoods).then(function(data) {
+d3.json(earthquakeData).then(function(data) {
     console.log(data);
     // Creating a GeoJSON layer with the retrieved data.
-    L.geoJson(data, {
-        style:myStyle,
-        onEachFeature: function(feature,layer) {
-            console.log(feature);
-            console.log(layer);
-            layer.bindPopup("<h3>Neighborhood: "+layer.feature.properties.AREA_NAME+"</h3>");
-        }
-    }).addTo(map);
+    L.geoJson(data).addTo(map);
 });
 
 
